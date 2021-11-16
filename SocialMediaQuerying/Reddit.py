25f20bca9e4950346a81c84b19318b9594a7006a
Neuman, Wesley <Wesley.Neuman@gentex.com>
2021-11-16 13:42:12 -0500

    Added Reddit api info outside of git and created main Reddit class

--- SocialMediaQuerying/Reddit.py
+++ SocialMediaQuerying/Reddit.py
@@ -1,1 +1,17 @@
 import praw
+import api_secrets as sec
+import SocialMediaQuerying.SubredditDefinitions.Subreddit as subreddit
+
+
+class Reddit(object):
+
+    def __init__(self):
+        self.subreddits = subreddit.generate_subreddit_dictionary()
+        self.rhandler = praw.Reddit(
+            client_id=sec.reddit_client,
+            client_secret=sec.reddit_secret,
+            password=sec.reddit_pswd,
+            user_agent="WNAGENT",
+            username=sec.reddit_user
+        )
+
